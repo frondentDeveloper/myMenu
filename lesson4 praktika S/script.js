40aae96f6705b1766366a83e240f76365ad73fc3
frondentDeveloper <96831971+frondentDeveloper@users.noreply.github.com>
2021-12-29 20:35:04 +0500

    Add files via upload

---
+++ lesson4 praktika S/script.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector(".loader");
+
+    setTimeout(() => {
+        loader.style.opacity ='0',
+            loader.style.display = 'none'
+    }, 2000)
+});
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tabheader__item');
+           tabsParent = document.querySelector('.tabheader__items');
+           tabContent = document.querySelectorAll('.tabcontent');
+
+
+     function hideTabContent() {
+         tabContent.forEach(item =>{
+             item.classList.add('none');
+             item.classList.remove('show', 'fade')
+         });
+         tabs.forEach(item => {
+             item.classList.remove('tabheader__item_active')
+         })
+     }
+     function showTabContent(i = 0) {
+         tabContent[i].classList.add('show', 'fade');
+         tabContent[i].classList.remove('none');
+         tabs[i].classList.add('tabheader__item_active')
+     }
+     hideTabContent();
+    showTabContent();
+
+
+    tabsParent.addEventListener('click', (event) => {
+        if (event.target && event.target.classList.contains('tabheader__item')){
+            tabs.forEach((item,  i)=>{
+               if (event.target == item){
+             hideTabContent();
+             showTabContent(i)
+}
+            })
+        }
+    })
+
+});
